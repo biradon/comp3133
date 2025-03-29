ba9213021bf63459bd8ff9050d376d8a7f9ad9d8
biradon <truongchauminhjps15@gmail.com>
2025-03-28 22:13:53 -0400

    Add functional for loginForm

--- assignment2/frontend/101411424_comp3133_assignment2/src/app/login-form/login-form.component.ts
+++ assignment2/frontend/101411424_comp3133_assignment2/src/app/login-form/login-form.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
 export class LoginFormComponent {
 
+  user = {
+    username: '',
+    password: ''
+  };
+
+  onSubmit() {
+    console.log(this.user); // Outputs: { username: 'user input', password: 'pass input' }
+    // Process this.user here
+  }
+
+
+  public loginClick(): void {
+    alert(`Click Login ${this.user.username}`)
+  }
+
+  public signUpClick(): void {
+    alert(`Click Signup ${this.user.username}`)
+  }
 }
+
